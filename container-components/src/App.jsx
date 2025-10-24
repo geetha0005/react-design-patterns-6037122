c4e32c476002faa1ef25a0ff1b39cac61f31d6d9
Geetha Chegonda <geetha.chegonda@zf.com>
2025-10-24 08:38:38 +0530

    uncontrolled

--- container-components/src/App.jsx
+++ container-components/src/App.jsx
@@ -1,45 +1,39 @@
-import axios from 'axios'
-import { CurrentUserLoader } from "./CurrentUserLoader"
-import { DataLoader } from "./DataLoader"
-import { ResourceLoader } from "./ResourceLoader"
-import { UserInfo } from "./UserInfo"
-import { UserLoader } from "./UserLoader"
+import axios from "axios";
+import { CurrentUserLoader } from "./CurrentUserLoader";
+import { DataLoader } from "./DataLoader";
+import { ResourceLoader } from "./ResourceLoader";
+import { UserInfo } from "./UserInfo";
+import { UserLoader } from "./UserLoader";
 
-const getServerData = url => async () => {
+const getServerData = (url) => async () => {
   const response = await axios.get(url);
-  return response.data
-}
+  return response.data;
+};
 
 const getTestData = () => {
   return {
-    id: '12345',
-    name: 'Blah blah',
-    age: 54,
-    hairColor: 'brown',
-    hobbies: ['swimming', 'bicycling', 'video games'],
-  }
-}
+    id: "12345",
+    name: "Geetha",
+    age: 24,
+    hairColor: "brown",
+    hobbies: ["swimming", "bicycling", "video games"],
+  };
+};
 
 function App() {
   return (
     <>
-    <DataLoader
-      getDataFn={getTestData}
-      resourceName="user"
-    >
-      <UserInfo />
-    </DataLoader>
-    <ResourceLoader
-      resourceUrl="/api/users/345"
-      resourceName="user"
-    >
-      <UserInfo />
-    </ResourceLoader>
-    <UserLoader userId="345">
-      <UserInfo />
-    </UserLoader>
+      <DataLoader getDataFn={getTestData} resourceName="user">
+        <UserInfo />
+      </DataLoader>
+      <ResourceLoader resourceUrl="/api/users/345" resourceName="user">
+        <UserInfo />
+      </ResourceLoader>
+      <UserLoader userId="345">
+        <UserInfo />
+      </UserLoader>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
