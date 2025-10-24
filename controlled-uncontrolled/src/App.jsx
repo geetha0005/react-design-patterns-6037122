4f241ec66cf7d2d2eaba18e66ddffd23efa58760
Geetha Chegonda <geetha.chegonda@zf.com>
2025-10-24 09:06:40 +0530

    controlledfrom

--- controlled-uncontrolled/src/App.jsx
+++ controlled-uncontrolled/src/App.jsx
@@ -1,11 +1,17 @@
-import { UncontrolledForm } from "./UncontrolledForm"
+import { ControlledForm } from "./ControlledForm";
+import { UncontrolledForm } from "./UncontrolledForm";
 
 function App() {
   return (
     <>
       <UncontrolledForm />
+      <ControlledForm
+        initialName="geetha"
+        initialAge="20"
+        initialHairColor="Brown"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
