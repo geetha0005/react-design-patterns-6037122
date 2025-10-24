a56dbf50d22513e29c6b4e066a563cf7a53ee8af
Geetha Chegonda <geetha.chegonda@zf.com>
2025-10-24 07:44:15 +0530

    resourceloader

--- container-components/src/App.jsx
+++ container-components/src/App.jsx
@@ -1,21 +1,29 @@
-import { CurrentUserLoader } from "./CurrentUserLoader"
-import { UserInfo } from "./UserInfo"
-import { UserLoader } from "./UserLoader"
+import { CurrentUserLoader } from "./CurrentUserLoader";
+import { ResourceLoader } from "./ResourceLoader";
+import { UserInfo } from "./UserInfo";
+import { UserLoader } from "./UserLoader";
 
 function App() {
   return (
+    // <>
+    // <UserLoader userId="345">
+    //   <UserInfo />
+    // </UserLoader>
+    // <UserLoader userId="234">
+    //   <UserInfo />
+    // </UserLoader>
+    // <UserLoader userId="123">
+    //   <UserInfo />
+    // </UserLoader>
+    // </>
     <>
-    <UserLoader userId="345">
-      <UserInfo />
-    </UserLoader>
-    <UserLoader userId="234">
-      <UserInfo />
-    </UserLoader>
-    <UserLoader userId="123">
-      <UserInfo />
-    </UserLoader>
+      <ResourceLoader>
+        ResourceUrl={`/api/users/345`}
+        resourceName="user"
+        <UserInfo />
+      </ResourceLoader>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
